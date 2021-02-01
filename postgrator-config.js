0b9c41523926e809868acb30d14bd2b452378aa8
Jessica <jmbaskerville1217@gmail.com>
2021-02-01 11:31:35 -0500

    testing

--- postgrator-config.js
+++ postgrator-config.js
@@ -5,9 +5,4 @@ module.exports = {
   "driver": "pg",
   "connectionString": process.env.DATABSE_URL,
   "ssl": process.env.NODE_ENV === "production",
-  "host": process.env.MIGRATION_DB_HOST,
-  "port": process.env.MIGRATION_DB_PORT,
-  "database": process.env.MIGRATION_DB_NAME,
-  "username": process.env.MIGRATION_DB_USER,
-  "password": process.env.MIGRATION_DB_PASS,
 }
