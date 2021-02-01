383de5010d0d29feb4421ba88e1ebbedf0e24f52
Jessica <jmbaskerville1217@gmail.com>
2021-02-01 11:44:20 -0500

    changes to db

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,6 +3,6 @@ require('dotenv').config();
 module.exports = {
   "migrationDirectory": "migrations",
   "driver": "pg",
-  "connectionString": process.env.DB_URL,
+  "connectionString": process.env.DATABASE_URL,
   "ssl": process.env.NODE_ENV === "production",
 }
